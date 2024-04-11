30ca940d2c3cb891ee2e2e56c94de21a17b69d26
GoDie4 <anthony10.reyes10@gmail.com>
2024-04-11 17:05:04 -0500

    Updates

--- src/components/TodoList.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const TodoList = (props) => {
-  return <ul className="flex gap-2 flex-col w-full justify-center mx-auto my-4">{props.children}</ul>;
-};
-
-export { TodoList };
